60b6dff4f7e5e07247329961649e2ebe80794b71
Chuba-Nwene. C <chinemelumchubanwene57@gmail.com>
2024-04-03 21:00:53 +0200

    add and style collaboration section

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,7 @@
 const Heading = ({ className, title }) => {
   return (
     <div
-      className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}
+      className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 text-center`}
     >
       {title && <h2 className="h2">{title}</h2>}
     </div>
