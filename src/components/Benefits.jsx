7e6c831a14873f099eb32b4c092a8a3c66cedbb9
Chuba-Nwene. C <chinemelumchubanwene57@gmail.com>
2024-04-03 23:09:11 +0200

    add and style services section, center header  text for  benefits section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -10,7 +10,7 @@ const Benefits = () => {
     <Section id="features">
       <div className="container relative z-2">
         <Heading
-          className="md:max-w-md lg:max-w-2xl"
+          className="md:max-w-md lg:max-w-2xl lg:text-center"
           title="Chat Smarter, Not Harder with Brainwave"
         />
 
